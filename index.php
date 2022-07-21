23293ee9832cf7cb287bcf6d3c0fc73c55c338de
Jacob Noe <jacobnoegarciadimas@gmail.com>
2022-07-21 16:15:59 -0500

    Se agregaron las funciones Get y Post en el archivo Cliente.php y también se modificó el archivo index.php agregando el Get y Post.

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php 
+    require_once "models/Cliente.php";
+
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    $method = $_SERVER['REQUEST_METHOD'];
+     if($method == "OPTIONS") {
+         die();
+     }
+
+    switch ($_SERVER['REQUEST_METHOD']) {
+
+        case 'GET':
+            if(isset($_GET['id'])) {
+                echo json_encode(Cliente::getWhere($_GET['id']));
+            } // end if 
+            else {
+                echo json_encode(Cliente::getAll());
+            } // end else
+            break;
+
+        case 'POST':
+            $datos = json_decode(file_get_contents('php://input'));
+            if($datos != NULL) {
+                if(Cliente::insert($datos->usuario, $datos->correo, $datos->contrasena, $datos->nacimiento, $datos->foto)) {
+                    http_response_code(200);
+                } // end if
+                else {
+                    http_response_code(400);
+                } // end else
+            } // end if
+            else{
+                http_response_code(405);
+            } // end  else
+            break;
+    }  // end while
---
+++ models/Cliente.php
@@ -0,0 +1,60 @@
+<?php
+    require_once "connection/Connection.php";
+
+    class Cliente {
+        
+        public static function getAll() {
+            $db = new Connection();
+            $query = "SELECT * FROM clientes";
+            $resultado = $db->query($query);
+            $datos = [];
+            if($resultado->num_rows) {
+                while($row = $resultado->fetch_assoc()) {
+                    $datos[] = [
+                        'id' => $row['id'],
+                        'usuario' => $row['usuario'],
+                        'correo' => $row['correo'],
+                        'contrasena' => $row['contrasena'],
+                        'nacimiento' => $row['nacimiento'],
+                        'foto' => $row['foto'],
+                    ];
+                }//end while
+                return $datos;
+            }//end if
+            return $datos;
+        }// end getAll acceder a toda la inf de la BD
+
+        public static function getWhere($id_cliente) {
+            $db = new Connection();
+            $query = "SELECT * FROM clientes WHERE id=$id_cliente";
+            $resultado = $db->query($query);
+            $datos = [];
+            if($resultado->num_rows) {
+                while($row = $resultado->fetch_assoc()) {
+                    $datos[] = [
+                        'id' => $row['id'],
+                        'usuario' => $row['usuario'],
+                        'correo' => $row['correo'],
+                        'contrasena' => $row['contrasena'],
+                        'nacimiento' => $row['nacimiento'],
+                        'foto' => $row['foto'],
+                    ];
+                }//end while
+                return $datos;
+            }//end if
+            return $datos;
+        }// end getWhere acceder a toda la inf de la BD
+
+        public static function insert($usuario, $correo, $contrasena, $nacimiento, $foto) {
+            $db = new Connection();
+            $query = "INSERT INTO clientes (usuario, correo, contrasena, nacimiento, foto)
+            VALUES('".$usuario."', '".$correo."', '".$contrasena."', '".$nacimiento."', '".$foto."')";
+            $db->query($query);
+            if($db->affected_rows) {
+                return  TRUE;
+            }//end if
+            return FALSE;
+        }//end insert
+
+    }//end class Client 
+ 
